f6d0ea3325d3178ccbd9acacd219e7838d5b0672
Adil Khan <adilkhandeveloper@gmail.com>
2024-10-18 08:48:03 -0700

    add escape button

--- app/components/CMenu.tsx
+++ app/components/CMenu.tsx
@@ -8,6 +8,7 @@ const CMenu: FC = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState<Command[] | null>();
   const [selected, setSelected] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
 
   const filter = (query: string): Command[] => {
     return commands.filter((command) => {
@@ -17,9 +18,14 @@ const CMenu: FC = () => {
 
   const navigation = useCallback(
     (event: KeyboardEvent) => {
+      if (event.key == "k" && (event.ctrlKey || event.metaKey)) {
+        setIsOpen((prev) => !prev);
+      }
+      if (event.key === "Escape") {
+        setIsOpen(false);
+      }
       if (results) {
         const length = results.length - 1;
-
         if (
           event.key === "ArrowUp" ||
           (event.key === "Tab" && event.shiftKey)
@@ -70,55 +76,60 @@ const CMenu: FC = () => {
     };
   }, [navigation]);
   return (
-    <motion.div
-      className="flex items-center justify-center overflow-hidden fixed w-screen h-screen select-none bg-[#e7e7e7e5]"
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-      }}
-    >
-      <motion.div
-        className="w-[640px] will-change-auto relative bg-white rounded-lg shadow-2xl"
-        initial={{ scale: 0.9, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ type: "spring", damping: 20, stiffness: 300 }}
-      >
-        <div className="h-16 flex items-center text-xl text-gray-400 border-gray-200 border-b">
-          <FiSearch className="ml-4" />
-          <input
-            placeholder="search commands ..."
-            type="text"
-            autoFocus
-            className="ml-4 outline-none w-full will-change-auto text-lg"
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-          />
-        </div>
+    <AnimatePresence>
+      {isOpen && (
+        <motion.dialog
+          className="flex items-center justify-center overflow-hidden fixed w-screen h-screen select-none bg-[#e7e7e7e5]"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+          }}
+          exit={{
+            opacity: 0,
+          }}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <motion.div
+            className="w-[640px] will-change-auto relative bg-white rounded-lg shadow-2xl"
+            initial={{ scale: 0.9, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ type: "spring", damping: 20, stiffness: 300 }}
+          >
+            <div className="h-16 flex items-center text-xl text-gray-400 border-gray-200 border-b">
+              <FiSearch className="ml-4" />
+              <input
+                placeholder="search commands ..."
+                type="text"
+                autoFocus
+                className="ml-4 outline-none w-full will-change-auto text-lg"
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}
+              />
+            </div>
 
-        <motion.ul className="flex overflow-y-auto overflow-x-hidden flex-col w-full transition-all will-change-auto max-h-[320px]">
-          <AnimatePresence initial={false}>
-            {results?.map((command, index) => (
-              <motion.div
-                key={command.text + index}
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: "auto" }}
-                exit={{ opacity: 0, height: 0 }}
-                transition={{ duration: 0.2 }}
-              >
-                <Command
-                  command={command}
-                  onMouseMove={() => setSelected(index)}
-                  selected={index === selected}
-                />
-              </motion.div>
-            ))}
-          </AnimatePresence>
-        </motion.ul>
-      </motion.div>
-    </motion.div>
+            <motion.ul className="flex overflow-y-auto overflow-x-hidden flex-col w-full transition-all will-change-auto max-h-[320px]">
+              <AnimatePresence initial={false}>
+                {results?.map((command, index) => (
+                  <motion.div
+                    key={command.text + index}
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{ duration: 0.2 }}
+                  >
+                    <Command
+                      command={command}
+                      onMouseMove={() => setSelected(index)}
+                      selected={index === selected}
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
+            </motion.ul>
+          </motion.div>
+        </motion.dialog>
+      )}
+    </AnimatePresence>
   );
 };
 
